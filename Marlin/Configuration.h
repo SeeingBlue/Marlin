3dbe9bff32fd6767a7c6e902641bd41a6cbdf183
SeeingBlue <seeingblue@seeingblue.us>
2021-01-24 20:05:19 -0500

    changed Default_Axis_Steps_per_Unit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -737,7 +737,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2560, 723.38 } // ncrmro
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 723.38 } // ncrmro
 
 /**
  * Default Max Feed Rate (mm/s)
